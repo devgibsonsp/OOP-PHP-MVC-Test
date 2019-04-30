5ec3ab0b6e53840fb32ca563bf6fe39ec14a2f95
Steven Gibson <devgibsonsp@gmail.com>
2019-04-29 21:31:33 -0400

    adding models

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
 
 // Define DB Params
-define();
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "turtles123");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost/test");
+
+
+?>
