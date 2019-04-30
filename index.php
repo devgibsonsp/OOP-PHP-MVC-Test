edbaa4f1d9f202a9959712cb0e888923904c74f5
Steven Gibson <devgibsonsp@gmail.com>
2019-04-29 20:26:58 -0400

    added home controller

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+// Include Config
+require('config.php');
 
-    require('config.php');
-    require('classes/Bootstrap.php');
+require('classes/Bootstrap.php');
+require('classes/Controller.php');
 
-    $bootstrap = new Bootstrap($_GET);
-    $controller = $bootstrap->createController();
-?>
+require('controllers/home.php');
+
+$bootstrap = new Bootstrap($_GET);
+$controller = $bootstrap->createController();
+if($controller){
+	$controller->executeAction();
+}
